04c0e4f5db18d2713efc71a88201076cdb7bb575
Camila Castro <cami@Camilas-MacBook-Air.local>
2024-08-01 18:46:20 -0300

    Agregar responsive a contacto

--- src/app/contactanos/Contacto.js
+++ src/app/contactanos/Contacto.js
@@ -5,40 +5,39 @@ import { FaMapMarkerAlt } from "react-icons/fa";
 
 const Contacto = () => {
     return (
-        <>
-        
-        
-        
-         <div className="mt-60">
-        <div className="flex items-center justify-center pb-3 text-3xl font-bold text-orange-400 ">REDES SOCIALES</div>
-        <div className="flex items-center justify-center text-4xl font-bold text-white ">LONG HUN</div>
+      <>
+        <div className="flex flex-col items-center gap-8 mb-10 md:mb-0 md:gap-0">      
+         <div className="mt-36">
+        <div className="flex items-center justify-center pb-3 text-lg font-bold text-orange-400 md:text-3xl ">REDES SOCIALES</div>
+        <div className="flex items-center justify-center text-2xl font-bold text-white md:text-4xl ">LONG HUN</div>
         </div>
-       <ul className="grid grid-cols-6 grid-rows-2 gap-4 p-20" style={{ gridTemplateRows: 'repeat(2, 450px)' }}>
+       <ul className="flex flex-col items-center gap-4 md:p-20 md:grid md:grid-cols-6 lg:lg-custom-grid-rows">
             <li className="col-span-2 row-span-1">
                 <Card imageSrc ="/assets/contactosImg/facebook.jpg" content="FACEBOOK" content2="JOSE KRAITZ"/>
             </li>
-            <li className="col-span-2 row-span-1">
+            <li className="md:col-span-2 md:row-span-1">
                 <Card  imageSrc ="/assets/contactosImg/instagramjose.jpg"  content="INSTAGRAM" content2="LONGHUN"/>
             </li>
-            <li className="col-span-2 row-span-1">
+            <li className="md:col-span-2 md:row-span-1">
                 <Card imageSrc={"/assets/contactosImg/longhunig.jpg"} content="YOUTUBE" content2="JOSE KRAITZ"/>  
             </li>
-            <li className="col-span-3 row-span-2">
+            <li className="md:col-span-3 md:row-span-2">
                 <Card2 imageSrc ="/assets/contactosImg/longhunig.jpg" content="INSTAGRAM" content2="_LONGHUN"/>
             </li>
-            <li className="col-span-3 row-span-2">
+            <li className="md:col-span-3 md:row-span-2">
                 <Card2 imageSrc ="/assets/contactosImg/tiktok.jpg" content="TIKTOK" content2="LONG.HUN3"/>
             </li>
        </ul>
+       </div>
 
-       <div className="flex items-center justify-center bg-black p-20">
-      <div className="flex space-x-8 ">
-        <div className="flex items-center space-x-4 mr-40">
-          <div className="bg-[#232323] rounded-full p-5">
-            <FaMapMarkerAlt className="text-white text-2xl" />
+       <div className="flex items-center justify-center p-10 bg-black md:p-20">
+      <div className="flex flex-col gap-10 md:gap-0 md:space-x-8 md:flex-row ">
+        <div className="flex space-x-4 lg:mr-40">
+          <div className="bg-[#232323] rounded-full flex items-center w-16 h-16 justify-center p-3 md:p-5">
+            <FaMapMarkerAlt className="text-base text-white md:text-2xl" />
           </div>
           <div>
-            <div className="text-orange-400 text-2xl font-bold">SEDE ASOCIACION CIVIL</div>
+            <div className="text-base font-bold text-orange-400 lg:text-2xl">SEDE ASOCIACION CIVIL</div>
             <div className="text-white">
               Asociación civil luchemos por ellos <br />
               Falucho S/N - Villa Itatí, Bernal, Quilmes
@@ -46,12 +45,12 @@ const Contacto = () => {
           </div>
         </div>
 
-        <div className="flex items-center space-x-4">
-          <div className="bg-[#232323] rounded-full p-5">
-            <FaMapMarkerAlt className="text-white text-2xl" />
+        <div className="flex space-x-4">
+          <div className="bg-[#232323] rounded-full flex items-center w-16 h-16 justify-center p-3 md:p-5">
+            <FaMapMarkerAlt className="text-base text-white md:text-2xl" />
           </div>
           <div>
-            <div className="text-orange-400 text-2xl font-bold">SEDE CENTRAL</div>
+            <div className="text-base font-bold text-orange-400 lg:text-2xl">SEDE CENTRAL</div>
             <div className="text-white">
               Chacabuco 835, Bernal, Quilmes
             </div>
