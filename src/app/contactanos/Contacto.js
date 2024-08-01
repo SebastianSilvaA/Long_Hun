9222d56446e499ac1120ef6b3604bf758ff67db0
SebastianSilvaA <sebastiansilvaaleman1@hotmail.com>
2024-08-01 16:53:23 -0300

    contactanos: falta ser responsive

--- src/app/contactanos/Contacto.js
+++ src/app/contactanos/Contacto.js
@@ -1,10 +1,14 @@
 import Card2 from "@/components/primary/card2";
 import "./styles.css"
 import Card from "@/components/primary/card"
+import { FaMapMarkerAlt } from "react-icons/fa";
 
 const Contacto = () => {
     return (
         <>
+        
+        
+        
          <div className="mt-60">
         <div className="flex items-center justify-center pb-3 text-3xl font-bold text-orange-400 ">REDES SOCIALES</div>
         <div className="flex items-center justify-center text-4xl font-bold text-white ">LONG HUN</div>
@@ -20,12 +24,41 @@ const Contacto = () => {
                 <Card imageSrc={"/assets/contactosImg/longhunig.jpg"} content="YOUTUBE" content2="JOSE KRAITZ"/>  
             </li>
             <li className="col-span-3 row-span-2">
-                <Card2 imageSrc ="/assets/contactosImg/longhunig.jpg" content="YOUTUBE" content2="JOSE KRAITZ"/>
+                <Card2 imageSrc ="/assets/contactosImg/longhunig.jpg" content="INSTAGRAM" content2="_LONGHUN"/>
             </li>
             <li className="col-span-3 row-span-2">
-                <Card2 imageSrc ="/assets/contactosImg/longhunig.jpg" content="YOUTUBE" content2="JOSE KRAITZ"/>
+                <Card2 imageSrc ="/assets/contactosImg/tiktok.jpg" content="TIKTOK" content2="LONG.HUN3"/>
             </li>
        </ul>
+
+       <div className="flex items-center justify-center bg-black p-20">
+      <div className="flex space-x-8 ">
+        <div className="flex items-center space-x-4 mr-40">
+          <div className="bg-[#232323] rounded-full p-5">
+            <FaMapMarkerAlt className="text-white text-2xl" />
+          </div>
+          <div>
+            <div className="text-orange-400 text-2xl font-bold">SEDE ASOCIACION CIVIL</div>
+            <div className="text-white">
+              Asociación civil luchemos por ellos <br />
+              Falucho S/N - Villa Itatí, Bernal, Quilmes
+            </div>
+          </div>
+        </div>
+
+        <div className="flex items-center space-x-4">
+          <div className="bg-[#232323] rounded-full p-5">
+            <FaMapMarkerAlt className="text-white text-2xl" />
+          </div>
+          <div>
+            <div className="text-orange-400 text-2xl font-bold">SEDE CENTRAL</div>
+            <div className="text-white">
+              Chacabuco 835, Bernal, Quilmes
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
 
 
         
