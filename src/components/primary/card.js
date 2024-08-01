999925523457c662f45d84db1cb641fc062d0127
SebastianSilvaA <sebastiansilvaaleman1@hotmail.com>
2024-08-01 19:57:51 -0300

    portafolio: footer: falta responsive

--- src/components/primary/card.js
+++ src/components/primary/card.js
@@ -4,11 +4,11 @@ const Card = ({ imageSrc, content, content2, imgClass }) => {
     return (
         <div className="flex items-center">
             <div className="inline-block overflow-hidden transition duration-300 transform shadow-xl hover:shadow-2xl hover:scale-105">
-                <img className={`w-auto h-80 object-cover ${imgClass}`} src={imageSrc} alt="" />
-                <div className="flex items-center justify-between p-4 mt-2 bg-customBlack">
+                <img className={`w-80 md:w-auto h-auto md:h-44 lg:h-80 object-cover ${imgClass}`} src={imageSrc} alt="" />
+                <div className="flex flex-col items-start justify-between gap-2 p-4 mt-2 lg:gap-0 lg:items-center lg:flex-row bg-customBlack">
                     <div>
                         <div className="text-xl text-orange-400">{content}</div>
-                        <div className="mt-2 text-3xl font-bold text-white">{content2}</div>
+                        <div className="mt-2 text-2xl font-bold text-white">{content2}</div>
                     </div>
                     <div>
                         <button className="px-4 py-2 font-semibold text-white transition duration-500 rounded-lg shadow-md text-md bg-customGray hover:shadow-lg transform-gpu hover:scale-110">
--- src/components/primary/card2.js
+++ src/components/primary/card2.js
@@ -7,12 +7,12 @@ const Card2 = ({ imageSrc, content, content2, imgClassName }) => {
     <div class="flex items-center">
       <div className="container overflow-hidden transition duration-300 transform shadow-xl max-w shadow-x hover:shadow-2xl hover:scale-105">
         <div className="overflow-hidden">
-          <img className={`w-auto h-auto ${imgClassName}`} src={imageSrc} alt="" />
+          <img className={`w-80 h-auto md:w-auto md:h-auto ${imgClassName}`} src={imageSrc} alt="" />
         </div>
-        <div className="flex items-center justify-between p-4 mt-2 bg-customBlack">
+        <div className="flex flex-col items-start justify-between p-4 mt-2 md:items-center md:flex-row bg-customBlack">
           <div>
             <div className="mt-5 ml-3 text-xl text-orange-400">{content}</div>
-            <div className="mt-2 mb-2 ml-2 text-3xl font-bold text-white">{content2}</div>
+            <div className="mt-2 mb-2 ml-2 text-2xl font-bold text-white">{content2}</div>
           </div>
           <div className="mr-3">
             <button className="px-4 py-2 font-semibold text-white transition duration-500 transform rounded-lg shadow-md text-md bg-customGray hover:shadow-lg hover:scale-110">
