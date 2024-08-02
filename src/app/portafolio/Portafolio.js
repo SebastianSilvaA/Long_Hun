3eb03868862124b121edb6c81b87998c79d94e9c
Camila Castro <cami@Camilas-MacBook-Air.local>
2024-08-02 07:28:39 -0300

    responsive

--- src/app/portafolio/Portafolio.js
+++ src/app/portafolio/Portafolio.js
@@ -12,27 +12,27 @@ const Portafolio = () => {
         
         <div className="mt-60">
 
-            <ul className="grid grid-cols-6 grid-rows-2 gap-4 p-10">
+            <ul className="grid grid-cols-6 grid-rows-4 gap-4 p-10 lg:grid-rows-2">
 
-            <li className="col-span-3 row-span-1" style={{ height: '300px' }}>
-                <img src="/assets/portafolioImg/p1.jpg" alt="Imagen 1" className="w-full h-full object-cover" />
+            <li className="col-span-6 row-span-1 lg:col-span-3" style={{ height: '300px' }}>
+                <img src="/assets/portafolioImg/p1.jpg" alt="Imagen 1" className="object-cover w-full h-full" />
             </li>
-            <li className="col-span-2 row-span-1" style={{ height: '300px' }}>
-                <img src="/assets/portafolioImg/p2.jpg" alt="Imagen 2" className="w-full h-full object-cover" />
+            <li className="col-span-4 row-span-2 lg:col-span-2 lg:row-span-1" style={{ height: '300px' }}>
+                <img src="/assets/portafolioImg/p2.jpg" alt="Imagen 2" className="object-cover w-full h-full" />
             </li>
             <li className="col-span-1 row-span-1" style={{ height: '300px' }}>
-                <img src="/assets/portafolioImg/p3.jpg" alt="Imagen 3" className="w-full h-full object-cover" />
+                <img src="/assets/portafolioImg/p3.jpg" alt="Imagen 3" className="object-cover w-full h-full" />
             </li>
             
 
             <li className="col-span-1 row-span-1" style={{ height: '300px' }}>
-                <img src="/assets/portafolioImg/p6.jpg" alt="Imagen 4" className="w-full h-full object-cover" />
+                <img src="/assets/portafolioImg/p6.jpg" alt="Imagen 4" className="object-cover w-full h-full" />
             </li>
             <li className="col-span-2 row-span-1" style={{ height: '300px' }}>
-                <img src="/assets/portafolioImg/p7.jpg" alt="Imagen 5" className="w-full h-full object-cover" />
+                <img src="/assets/portafolioImg/p7.jpg" alt="Imagen 5" className="object-cover w-full h-full" />
             </li>
             <li className="col-span-3 row-span-1" style={{ height: '300px' }}>
-                <img src="/assets/portafolioImg/p4.jpg" alt="Imagen 6" className="w-full h-full object-cover" />
+                <img src="/assets/portafolioImg/p4.jpg" alt="Imagen 6" className="object-cover w-full h-full" />
             </li>
             </ul>
             
