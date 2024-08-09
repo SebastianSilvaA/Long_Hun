2231b6dc4faa9932be15d95057c38ea9a9ed5f0e
SebastianSilvaA <sebastiansilvaaleman1@hotmail.com>
2024-08-09 15:27:59 -0300

    popup2

--- src/components/primary/pop.js
+++ src/components/primary/pop.js
@@ -1,15 +1,29 @@
 "use client";
 
-import { useState, useEffect } from 'react';
-import { Modal, ModalFooter } from 'flowbite-react';
-import { FaTimes } from 'react-icons/fa'; // Asegúrate de tener react-icons instalado
+import { useState, useEffect, useRef } from 'react';
+import { Modal } from 'flowbite-react';
+import { FaTimes } from 'react-icons/fa';
 import Popbutton from '../secondary/popbutton';
 
 export default function Popup() {
   const [show, setShow] = useState(false);
+  const modalRef = useRef(null);
 
   useEffect(() => {
     setShow(true);
+  }, []);
+
+  const handleClickOutside = (event) => {
+    if (modalRef.current && !modalRef.current.contains(event.target)) {
+      setShow(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
   }, []);
 
   return (
@@ -18,18 +32,18 @@ export default function Popup() {
       onClose={() => setShow(false)}
       className="bg-transparent backdrop-blur-sm"
     >
-      <button
-        onClick={() => setShow(false)}
-        className="absolute top-3 right-3 text-white bg-transparent hover:bg-gray-200 hover:text-gray-700 p-1 rounded-full"
-      >
-        <FaTimes />
-      </button>
-      <Modal.Body className='rounded-md bg-customBlack'>
-        <img src="/assets/popup.jpg" alt="Imagen" className="w-full h-auto" />
-      </Modal.Body>
-      <ModalFooter>
-        <Popbutton />
-      </ModalFooter>
+      <div ref={modalRef} className="relative">
+        <button
+          onClick={() => setShow(false)}
+          className="absolute top-3 right-3 text-white bg-transparent hover:bg-gray-200 hover:text-gray-700 p-1 rounded-full"
+        >
+          <FaTimes />
+        </button>
+        <Modal.Body className=" rounded-lg bg-customBlack flex flex-col items-center justify-center space-y-4">
+          <img src="/assets/popup.jpg" alt="Imagen" className="w-full h-auto" />
+          <Popbutton onClick={() => alert('Te has inscrito!')} />
+        </Modal.Body>
+      </div>
     </Modal>
   );
 }
--- src/components/secondary/popbutton.js
+++ src/components/secondary/popbutton.js
@@ -1,11 +1,12 @@
+"use client";
 
-
-const Popbutton = () => {
-    return (
-        <>
-        <h1 className="bg-orange-400">Popbutton</h1>
-        </>
-    )
+export default function Button({ onClick = () => alert('Botón clicado') }) {
+  return (
+    <button
+      onClick={onClick}
+      className="font-bold bg-orange-400 text-white px-4 py-2 rounded hover:bg-blue-600 transition"
+    >
+      Inscríbete
+    </button>
+  );
 }
-
-export default Popbutton
