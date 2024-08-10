f533468340518fed5db17431ded2a37d7607646e
SebastianSilvaA <sebastiansilvaaleman1@hotmail.com>
2024-08-10 15:55:21 -0300

    a

--- src/app/torneos/Torneos.js
+++ src/app/torneos/Torneos.js
@@ -2,7 +2,13 @@ import HeaderWithVideo from "@/components/primary/TorneoHeader"
 
 const Torneos = () => {
     return(
+        <>
         <HeaderWithVideo />
+        <div>6° EDICIÓN TORNEO AMISTAD - 22 DE SEPTIEMBRE 2024, QUILMES - ARGENTINA</div>
+        <div className="flex justify-center ">
+            <img src="/assets/torneo.jpg" className="size-2/5"></img>
+        </div>
+        </>
     )
 }
 
