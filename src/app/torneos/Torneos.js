4ad47d9616f391581b308850d81dda40bf575ec1
SebastianSilvaA <sebastiansilvaaleman1@hotmail.com>
2024-08-10 17:14:26 -0300

    torneo:falta responsive

--- src/app/torneos/Torneos.js
+++ src/app/torneos/Torneos.js
@@ -1,15 +1,151 @@
-import HeaderWithVideo from "@/components/primary/TorneoHeader"
+import HeaderWithVideo from "@/components/primary/TorneoHeader";
 
 const Torneos = () => {
-    return(
-        <>
-        <HeaderWithVideo />
-        <div>6° EDICIÓN TORNEO AMISTAD - 22 DE SEPTIEMBRE 2024, QUILMES - ARGENTINA</div>
-        <div className="flex justify-center ">
-            <img src="/assets/torneo.jpg" className="size-2/5"></img>
-        </div>
-        </>
-    )
-}
+  return (
+    <>
+      <HeaderWithVideo />
+      <div className="  mt-10 mb-10 text-3xl font-bold t text-center text-white">
+      <span className="text-orange-400">6° EDICIÓN TORNEO AMISTAD </span> - 22 DE SEPTIEMBRE 2024, QUILMES - ARGENTINA
+      </div>
 
-export default Torneos
+      <div className="flex justify-center">
+       <img  className="size-2/5" src="/assets/popup.jpg"></img>
+      </div>
+
+      <div className="container ml-16 mt-10">
+      <div className="text-white text-2xl">
+      El 22 de septiembre de 2024, a partir de las 09:00 am se llevará a cabo la 6° Edición del <span className="text-orange-400">Torneo Amistad</span> de artes marciales chinas, en el Club Libertad de Don Bosco, Gral. Roca 2200, Don Bosco.
+      </div>
+      <div className="text-white text-3xl mb-6">
+        <div className="font-bold text-orange-400 mt-10 mb-5">Precios:</div>
+        <ul className="list-disc list-inside">
+          <li>(1) Categoría: 10.000</li>
+          <li>Categorías adicionales: 3.000</li>
+          <li>Formas por equipo: 20.000</li>
+          <li>ENTRADAS GENERALES: 2.000</li>
+          <li>ANTICIPADAS EN EL LUGAR: 2.500</li>
+        </ul>
+      </div>
+      </div>
+
+     
+
+      <div className="text-white text-3xl mb-6 ml-16">
+        <div className="font-bold text-orange-400 text-4xl mb-4">REGLAMENTO COMBATE AL PUNTO</div>
+        <div>
+          <p className="mb-4">A. EQUIPO DE PROTECCIONES OBLIGATORIO PARA COMBATE A PUNTOS:</p>
+          <ul className="list-disc list-inside">
+            <li>Cabezal o casco.</li>
+            <li>Guantes.</li>
+            <li>En caso de usar zapatillas, deben ser las reglamentarias.</li>
+            <li>Protector bucal.</li>
+            <li>Protector inguinal masculino y femenino.</li>
+            <li>Protector de pecho femenino.</li>
+            <li>Canilleras o espinilleras.</li>
+            <li>El uso de pechera de taekwondo, es opcional y no obligatorio.</li>
+          </ul>
+
+          <p className="mb-10 mt-10">B. ZONAS DEL CUERPO VÁLIDAS EN LA MARCACIÓN DE PUNTOS:</p>
+          <ul className="list-disc list-inside">
+            <li>Zona alta (cabezal).</li>
+            <li>Zona media o tronco (costado, pecho y abdomen).</li>
+          </ul>
+
+          <p className="mb-10 mt-10">C. GOLPES PROHIBIDOS:</p>
+          <ul className="list-disc list-inside">
+            <li>Puños rectos al triángulo de la cara y golpes de gancho.</li>
+            <li>Patadas por debajo del cinturón y patadas a la espalda directa.</li>
+            <li>En caso de usar zapatillas, deben ser las reglamentarias.</li>
+            <li>
+              Habiendo coach, luego de la primera advertencia, la segunda será
+              contabilizada como punto en contra.
+            </li>
+          </ul>
+
+          <p className="mb-10 mt-10">D. SALIDAS:</p>
+          <p className="">
+            Si el competidor sale del área de combate tres veces, será
+            penalizado con un punto menos. Si en el momento de marcar puntos a
+            un competidor dentro del tatami y este producto de la acción sale
+            del área de combate, el o los puntos son válidos, ya que fue hecha
+            la marcación de puntos dentro del área de combate. Por el
+            contrario, si un atleta marca un punto fuera del área no se sumará
+            la técnica a favor.
+          </p>
+
+          <p className="mb-10 mt-10">
+            E. AMONESTACIONES O FALTAS: en caso de infracciones por parte del
+            competidor, estas serán penalizadas con amonestaciones.
+          </p>
+          <ul className="list-disc list-inside">
+            <li>
+              Una (1) amonestación de haber un empate al final del combate,
+              determinará como perdedor al competidor amonestado.
+            </li>
+            <li>
+              Dos (2) amonestaciones en un mismo combate, será causal de un
+              punto menos para el competidor penalizado.
+            </li>
+            <li>
+              Tres (3) amonestaciones en un mismo combate, será causal de
+              descalificación para el competidor penalizado.
+            </li>
+          </ul>
+
+          <p className="mb-10 mt-10">G. DURACIÓN DEL COMBATE:</p>
+          <p className="">
+            Una vez iniciada la categoría por ningún motivo puede ingresar un
+            atleta a la misma El combate a puntos en las categorías de colores
+            será de 1 minuto y medio. En las categorías de cintas negras será de
+            2 minutos. <span className="text-orange-400 font-bold">TODO COMBATE SERÁ A 5 PUNTOS</span>. <span className="underline decoration-white">En caso de finalizar el
+            combate y exista un empate, este se decidirá en muerte súbita
+            teniendo en cuenta salidas y advertencias en contra o el primer
+            punto gana.</span>
+          </p>
+
+          <p className="mb-10 mt-10">F. CONDUCTA ANTIDEPORTIVA:</p>
+          <p className="text-white">
+            Una vez iniciada la categoría por ningún motivo puede ingresar un
+            atleta a la misma. Si llegase a existir una conducta antideportiva
+            por parte de algún atleta, coach, entrenador, árbitro y personas en
+            general, considerada una grave falta de respeto al evento, a los
+            competidores o al público presente, el atleta o involucrado será
+            inmediatamente descalificado y expulsado del evento en curso.
+          </p>
+
+          <p className="mb-10 mt-10">H. COACH:</p>
+          <p className="text-white">
+            En todas las divisiones y categorías de combate está permitido el
+            coach para dirigir a su competidor, siendo el único autorizado a
+            estar en el área como así hablar con el 4° juez de ser necesario,
+            desde principiantes a cintas negras. El Coach deberá tramitar con
+            anticipación su credencial ante la organización.
+          </p>
+
+          <p className="mb-10 mt-10">I. Áreas:</p>
+          <ul className="list-disc list-inside">
+            <li>Todas las áreas de combate tendrán 4 Jueces (un central, dos espejos y un cuarto como veedor o reemplazo ante rotaciones), en caso de haber atletas en el área correspondiente a la escuela de uno de los jueces, se rotará a los fines de aumentar el nivel de objetividad en el arbitraje. </li>
+          </ul>
+          <p className="text-orange-400 mt-10 ">
+            LA ORGANIZACIÓN DEL EVENTO CUENTA CON UN EQUIPO DE ÁRBITROS Y JUECES, ASÍ COMO EQUIPOS DE MESAS DE CONTROL CON EL FIN DE DARLE CELERIDAD Y OBJETIVIDAD AL EVENTO, PERMITIENDO CON ELLO LIBERAR A LOS MAESTROS, PROFESORES Y DIRECTORES DE ESCUELAS, DE TAL MODO QUE DISFRUTEN DEL EVENTO Y PUEDAN ACOMPAÑAR A SUS ALUMNOS DURANTE EL DESARROLLO.
+          </p>
+
+          <p className=" mt-7 text-orange-400 underline decoration-orange-400">
+            SALVO EL COACH, NINGÚN OTRO PARTICIPANTE, INCLUSIVE MAESTRO, PODRÁ
+            ESTAR EN LA ZONA DE COMPETENCIA NI DAR INDICACIONES, PUESTO QUE,
+            SERÁ MOTIVO DE DESCALIFICACIÓN DEL COMPETIDOR.
+          </p>
+          <a
+            href="/assets/reglamento.pdf"
+            download="reglamento-torneo.pdf"
+            className=" mt-10 bg-orange-400 text-white font-bold py-2 px-4 rounded inline-block text-center"
+            >
+            MAS SOBRE EL REGLAMENTO
+            </a>
+         </div>
+      </div>
+    </>
+  );
+};
+
+export default Torneos;
